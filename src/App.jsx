34431d0d10a0f38bb5a80180758bcaba2d2fb06b
sheeba <sheebabalan2705@gmail.com>
2024-06-06 19:07:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import TodoList from './Components/TodoList';
+function App() {c
+ return (
+ <div className="App">
+ <TodoList />
+ </div>
+ ); 
+}
+export default App;
